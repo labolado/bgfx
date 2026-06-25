5b96a87f58294f03e7e5e548309e2f91055a95ee
Labo Lado <laboladoapp@gmail.com>
2026-06-25 14:26:39 +0800

    restore: add back platform.h (removed in upstream merge)

---
+++ include/bgfx/platform.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2011-2026 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
+ */
+
+#ifndef BGFX_PLATFORM_H_HEADER_GUARD
+#define BGFX_PLATFORM_H_HEADER_GUARD
+
+// NOTICE:
+// This header file contains platform specific interfaces. It is only
+// necessary to use this header in conjunction with creating windows.
+
+#include "bgfx.h"
+
+#if !BGFX_IDL_CPP
+
+namespace bgfx
+{
+	/// Render frame enum.
+	///
+	/// @attention C99's equivalent binding is `bgfx_re... (4702 bytes total)
+[full output: ~/Library/Application Support/rtk/tee/1782368066_curl.log]
